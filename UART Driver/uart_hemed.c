7d0609fb52e3ac5e8d048e17fb25e504a4660794
Mohammed Sayed hemed <mohammedhemed33@users.noreply.github.com>
2017-09-04 00:39:01 +0200

    Update uart_hemed.c

--- UART Driver/uart_hemed.c
+++ UART Driver/uart_hemed.c
@@ -1,9 +1,6 @@
-
-
 /*
-my uart library :) 
-
-
+uart library to interface AVR UART Module 
+mohammed hemed
 */
 #include "uart_hemed.h"
 void UART_init (unsigned int baud)
@@ -24,30 +21,24 @@ void UART_init (unsigned int baud)
 	UCSRC = (1<<URSEL)|(3<<UCSZ0);
 }
 
-
-
 void uart_send_byte (const unsigned char byte) {
-
 while (!(UCSRA & (1<<TXC))) ; 
 UDR = byte ; 
-
 } 
 
 unsigned char uart_rec_byte  (void) 
 {
 while (!(UCSRA & (1<<RXC))) ;
 return UDR ; 
-
 }
+
 void uart_send_string (const unsigned char *str)  
 {
 unsigned char i = 0 ; 
-
 while (str [i] != '\0')
 {
 uart_send_byte (str[i]) ; 
 i++ ; 
-
 }
 }
 
@@ -63,13 +54,12 @@ str [i] = uart_rec_byte ();
 }
 str[i] = '\0' ; 
 }
-/***********************************************************************************************/ 
+/***********************************************************************************/ 
 /*
 If you will use interrupt instead of polling you should 
 have two volatie variables : 
 volatile unsigned char recieved_data = 0 ; 
 volatile unsigned char transmit_data = 0 ;
-
 */
 // This is interrupt service routine for sending one character 
 // ----------------------------------------------------------------
@@ -85,22 +75,3 @@ ISR (USART_RXC_vect)
 recieved_data = UDR ; 	
 PORTB ^= 1<<PB1 ;			// Led indicator change its state when Recieve  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
